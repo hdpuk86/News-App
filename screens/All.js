a893ba8eb79cfc118923f121df04cad6db075573
hdpuk86 <hayley.d.prior@gmail.com>
2022-01-03 15:54:28 +0000

    set up API call

--- screens/All.js
+++ screens/All.js
@@ -1,5 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { View } from 'react-native';
+import { services } from '../services/services';
+
 export default function All() {
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    services('general')
+      .then(data => {
+        setNewsData(data)
+      })
+      .catch(error => {
+        alert(error)
+      })
+  }, []);
+
   return (
-    <></>
+    <View>
+
+    </View>
   );
 }
